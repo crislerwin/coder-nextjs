025921d8fac414827489987d3ce7941460310c4f
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-07-20 21:44:33 -0300

    chore: create login page & dasboard and put components inside

--- src/components/userRegister.tsx
+++ src/components/userRegister.tsx
@@ -7,19 +7,16 @@ export const UserRegister = () => {
   const router = useRouter();
   async function handleSignIn(user: User) {
     const { data } = await api.post('/user', user);
-
     if (data) {
       window.alert(
         `Usuario ${user.name} ${user.lastname} cadastrado com sucesso`,
       );
-      router.push('/table');
     }
   }
 
   return (
-    <div className="min-h-screen flex flex-col justify-center   ">
-      <div className="min-h-screen  text-center py-6 flex flex-col sm:py-12">
-        <h1 className="font-bold text-4xl m-10">Cadastro</h1>
+    <div className=" flex flex-col justify-center">
+      <div className=" text-center py-6 flex flex-col sm:py-12">
         <div className="relative py-2 w-10/12 max-w-xl sm:mx-auto">
           <div className="relative p-8 bg-white shadow-sm sm:rounded-xl">
             <form className="w-full" onSubmit={handleSubmit(handleSignIn)}>
@@ -29,11 +26,11 @@ export const UserRegister = () => {
                   type="text"
                   id="name"
                   required
-                  className="peer pt-8 border border-gray-200 focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
+                  className="peer pt-8 border border-gray-400 hover:border-gray-900  focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
                   placeholder="Name"
                 />
                 <label className="peer-placeholder-shown:opacity-100   opacity-75 peer-focus:opacity-75 peer-placeholder-shown:scale-100 scale-75 peer-focus:scale-75 peer-placeholder-shown:translate-y-0 -translate-y-3 peer-focus:-translate-y-3 peer-placeholder-shown:translate-x-0 translate-x-1 peer-focus:translate-x-1 absolute top-0 left-0 px-3 py-5 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out">
-                  <a className="text-black font-semibold ">Nome</a>
+                  <a className="text-black font-bold ">Nome</a>
                 </label>
               </div>
               <div className="mb-5 relative">
@@ -42,10 +39,10 @@ export const UserRegister = () => {
                   type="text"
                   id="lastname"
                   required
-                  className="peer pt-8 border border-gray-200 focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
+                  className="peer pt-8 border border-gray-400 hover:border-gray-900  focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
                 />
                 <label className="peer-placeholder-shown:opacity-100   opacity-75 peer-focus:opacity-75 peer-placeholder-shown:scale-100 scale-75 peer-focus:scale-75 peer-placeholder-shown:translate-y-0 -translate-y-3 peer-focus:-translate-y-3 peer-placeholder-shown:translate-x-0 translate-x-1 peer-focus:translate-x-1 absolute top-0 left-0 px-3 py-5 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out">
-                  <a className="text-black font-semibold ">Sobrenome</a>
+                  <a className="text-black font-bold ">Sobrenome</a>
                 </label>
               </div>
               <div className="mb-5 relative">
@@ -54,11 +51,11 @@ export const UserRegister = () => {
                   type="text"
                   id="role"
                   required
-                  className="peer pt-8 border border-gray-200 focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
+                  className="peer pt-8 border border-gray-400 hover:border-gray-900  focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
                   placeholder="role"
                 />
                 <label className="peer-placeholder-shown:opacity-100   opacity-75 peer-focus:opacity-75 peer-placeholder-shown:scale-100 scale-75 peer-focus:scale-75 peer-placeholder-shown:translate-y-0 -translate-y-3 peer-focus:-translate-y-3 peer-placeholder-shown:translate-x-0 translate-x-1 peer-focus:translate-x-1 absolute top-0 left-0 px-3 py-5 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out">
-                  <a className="text-black font-semibold ">Cargo</a>
+                  <a className="text-black font-bold ">Cargo</a>
                 </label>
               </div>
               <div className="mb-5 relative">
@@ -67,17 +64,17 @@ export const UserRegister = () => {
                   type="email"
                   id="email"
                   required
-                  className="peer pt-8 border border-gray-200 focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
+                  className="peer pt-8 border border-gray-500 hover:border-gray-900 focus:outline-none rounded-md focus:border-gray-500 focus:shadow-sm w-full p-3 h-16 placeholder-transparent"
                   placeholder="email"
                 />
                 <label className="peer-placeholder-shown:opacity-100   opacity-75 peer-focus:opacity-75 peer-placeholder-shown:scale-100 scale-75 peer-focus:scale-75 peer-placeholder-shown:translate-y-0 -translate-y-3 peer-focus:-translate-y-3 peer-placeholder-shown:translate-x-0 translate-x-1 peer-focus:translate-x-1 absolute top-0 left-0 px-3 py-5 h-full pointer-events-none transform origin-left transition-all duration-100 ease-in-out">
-                  <a className="text-black font-semibold ">Email</a>
+                  <a className="text-black font-bold ">Email</a>
                 </label>
               </div>
 
               <button
                 type="submit"
-                className="w-full bg-indigo-600 text-white p-3 rounded-md"
+                className="w-full bg-blue-900 hover:bg-blue-800  text-white p-3 rounded-md"
               >
                 <a className="b text-white px-4 py-2">Enviar</a>
               </button>
--- src/components/userTable.tsx
+++ src/components/userTable.tsx
@@ -1,8 +1,10 @@
 import { useFetch } from '../hooks/useFetch';
 import { User } from '../interfaces/User';
 import api from '../services/api';
+import { useRouter } from 'next/router';
 export const UserTable: React.FC<{}> = () => {
   const { data, error } = useFetch<User[]>('/user');
+  const router = useRouter();
   if (!data) {
     return <p>Loading...</p>;
   }
@@ -10,26 +12,25 @@ export const UserTable: React.FC<{}> = () => {
     return <p>Error: {error.message}</p>;
   }
   return (
-    <div className=" min-h-screen flex flex-col justify-center  text-center m-4 ">
-      <h1 className="m-6 font-bold text-4xl">Tabela de Usuários</h1>
+    <div className="  flex flex-col justify-center text-center m-4  ">
       <table className="max-w-4xl mx-auto table-auto ">
-        <thead className="justify-between">
-          <tr className="bg-indigo-600 ">
-            <th className="px-6 py-2">
+        <thead className="justify-between ">
+          <tr className="bg-blue-800 ">
+            <th className="px-12 py-2">
               <span className="text-gray-100 font-semibold">Nome</span>
             </th>
-            <th className="px-6 py-2">
+            <th className="px-12 py-2">
               <span className="text-gray-100 font-semibold">Sobrenome</span>
             </th>
-            <th className="px-6 py-2">
+            <th className="px-12 py-2">
               {' '}
               <span className="text-gray-100 font-semibold">Cargo</span>
             </th>
-            <th className="px-6 py-2">
+            <th className="px-12 py-2">
               {' '}
               <span className="text-gray-100 font-semibold">Email</span>
             </th>
-            <th className="px-6 py-2">
+            <th className="px-12 py-2">
               {' '}
               <span className="text-gray-100 font-semibold">Ações</span>
             </th>
@@ -40,24 +41,23 @@ export const UserTable: React.FC<{}> = () => {
             <td className="text-center ml-2 font-semibold">
               <span>{user.name}</span>
             </td>
-            <td className="px-6 py-2 font-semibold">
+            <td className="px-12 py-2 font-semibold">
               <span>{user.lastname}</span>
             </td>
-            <td className="px-6 py-2 font-semibold">
+            <td className="px-12 py-2 font-semibold">
               <span>{user.role}</span>
             </td>
-            <td className="px-6 py-2 font-semibold">
+            <td className="px-12 py-2 font-semibold">
               <span>{user.email}</span>
             </td>
-            <td className="px-6 py-2">
+            <td className="px-12 py-2">
               <a
-                className=" font-semibold hover:bg-gray-100  text-red-400 hover:text-red-600 hover:font-medium cursor-pointer "
-                onClick={async () => {
-                  await api.delete(`/user/${user.id}`).then((res) => {
+                className=" font-semibold   text-red-400 hover:text-red-600 hover:font-medium cursor-pointer "
+                onClick={() => {
+                  api.delete(`/user/${user.id}`).then((res) => {
                     window.alert(
                       `Usuário ${user.name} ${user.lastname} excluido com sucesso`,
                     );
-                    return res.data;
                   });
                 }}
               >
